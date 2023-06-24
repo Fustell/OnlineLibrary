80ff72a643d9dde491f3655bea7adaa911770fe7
Dmytro Simonov <simichdimon@gmail.com>
2023-06-24 11:04:35 +0300

    Added main urls, views and templates

--- Main/urls.py
+++ Main/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from Main import views
+from .views import AuthorBookListView, AuthorListView, BookListView, CategoryListView, BookDetailView
 
 urlpatterns = [
-    path('', views.index),
+    path('', BookListView.as_view()),  # home page 
+    path('authors/', AuthorListView.as_view()),  # authors list 
+    path('authors/<slug:author_books>/', AuthorBookListView.as_view()),  # list  of books of one author
+    path('categories/', CategoryListView.as_view()),  # categories list 
+    path('<slug:book_slug>/', BookDetailView.as_view()),  # details of certain book
 ]
